b8654a6d625dc18afc952f7ea1a5a1cb80f99768
Alexey Kolosov <alexey.kolosov@gmail.com>
2012-11-20 10:58:18 +0600

    Небольшие правки с правами пользователей

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,10 +3,10 @@ class SearchController < ApplicationController
   def index
   	if params[:query]
       if (@current_user.has_role? :admin)
-        @servers = Server.where("name like '%#{params[:query]}%' or description like '%#{params[:query]}%'")
   		  @commands = Command.where("name like '%#{params[:query]}%' or description like '%#{params[:query]}%'")
       end
 
+      @servers = Server.where("name like '%#{params[:query]}%' or description like '%#{params[:query]}%'")
   		@problems = Problem.where("name like '%#{params[:query]}%' or description like '%#{params[:query]}%'")
   		@solutions = Solution.where("name like '%#{params[:query]}%' or description like '%#{params[:query]}%'")
 
