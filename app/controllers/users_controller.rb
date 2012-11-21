6d7f4c29f1196da83dbb7bd45b314b7bd801d520
Alexey Kolosov <alexey.kolosov@gmail.com>
2012-11-21 17:12:52 +0600

    Небольшой багофикс

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -67,7 +67,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.update_attributes(params[:user])
-        format.html { redirect_to(@user, :notice => 'Пользователь успешно обновлен.') }
+        format.html { redirect_to(:users, :notice => 'Пользователь успешно обновлен.') }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
