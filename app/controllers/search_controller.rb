f055279daa7ca52e2ab785cb2e3e25b070d6154e
Alexey Kolosov <alexey.kolosov@gmail.com>
2012-11-29 10:41:24 +0600

    Поиск по журналу аудита для админов

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,6 +7,10 @@ class SearchController < ApplicationController
   		@solutions = Solution.where("name like '%#{params[:query]}%' or description like '%#{params[:query]}%'")
       @documents = Document.where("title like '%#{params[:query]}%' or body like '%#{params[:query]}%'")
 
+      if current_user.has_role? :admin
+        @audits = Audit.where("audited_changes like '%#{params[:query]}%' or comment like '%#{params[:query]}%'").order('created_at DESC')
+      end
+
   	  respond_to do |format|
 	      format.html # index.html.erb
 	      format.json { render json: @problems }
