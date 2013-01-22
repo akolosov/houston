5377994521cc3e1d2222fd1bd3304b3ec32034e5
Alexey Kolosov <alexey.kolosov@gmail.com>
2013-01-22 17:09:16 +0600

    Нормальная вьюшка для загруженных файлов, проработал хелперы для них

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 module ApplicationHelper
 
   def glyph(*names)
@@ -36,14 +37,39 @@ module ApplicationHelper
   end
   
   def link_to_attach_for_incedent(incedent, attach)
-   link_to glyph(:file)+' '+attach.name+' ('+attach.size.to_s+')', "#{root_url}uploads/incedents/#{incedent.id}/"+attach.name, title: attach.description,  class: 'btn btn-mini'
+   link_to glyph(:file)+' '+attach.name+' ('+attach.size.to_s+')', url_to_attach_for_incedent(incedent, attach), title: attach.description,  class: 'btn btn-mini'
   end
 
   def link_to_attach_for_document(document, attach)
-   link_to glyph(:file)+' '+attach.name+' ('+attach.size.to_s+')', "#{root_url}uploads/documents/#{document.id}/"+attach.name, title: attach.description,  class: 'btn btn-mini'
+   link_to glyph(:file)+' '+attach.name+' ('+attach.size.to_s+')', url_to_attach_for_document(document, attach), title: attach.description,  class: 'btn btn-mini'
   end
 
   def link_to_attach_for_comment(comment, attach)
-   link_to glyph(:file)+' '+attach.name+' ('+attach.size.to_s+')', "#{root_url}uploads/comments/#{comment.id}/"+attach.name, title: attach.description,  class: 'btn btn-mini'
+   link_to glyph(:file)+' '+attach.name+' ('+attach.size.to_s+')', url_to_attach_for_comment(comment, attach), title: attach.description,  class: 'btn btn-mini'
+  end
+
+  def url_to_attach_for_incedent(incedent, attach)
+   "#{root_url}uploads/incedents/#{incedent.id}/"+attach.name
+  end
+
+  def url_to_attach_for_document(document, attach)
+   "#{root_url}uploads/documents/#{document.id}/"+attach.name
+  end
+
+  def url_to_attach_for_comment(comment, attach)
+   "#{root_url}uploads/comments/#{comment.id}/"+attach.name
+  end
+
+  def url_to_attach(attach)
+    (!attach.incedents.empty? ? url_to_attach_for_incedent(attach.incedents.first, attach) :
+      (!attach.comments.empty? ? url_to_attach_for_comment(attach.comments.first, attach) :
+        (!attach.documents.empty? ? url_to_attach_for_document(attach.documents.first, attach) : '#')))
+  end
+  
+  def link_to_attach_source(attach)
+    link_to glyph(:inbox)+' Источник', (!attach.incedents.empty? ? attach.incedents.first : 
+    (!attach.comments.empty? ? (!attach.comments.first.documents.empty? ? document_url(attach.comments.first.documents.first, anchor: attach.comments.first.id.to_s) :
+      (!attach.comments.first.incedents.empty? ? incedent_url(attach.comments.first.incedents.first, anchor: attach.comments.first.id.to_s) : '')) :
+    (!attach.documents.empty? ? attach.documents.first : ''))), class: 'btn btn-mini'
   end
 end
