8fbd1b1e9ce2b9ac0d39d94e30406e0cf5a53420
Alexey Kolosov <alexey.kolosov@gmail.com>
2013-04-25 22:00:17 +0600

    Фикс проблемы при удалении пользователя

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,13 +10,23 @@ class User < ActiveRecord::Base
 
   has_many :documents, dependent: :destroy
 
-  has_many :owned_incedents, dependent: :destroy, foreign_key: 'initiator_id'
+  before_destroy { |record| Document.update_all "user_id = 1", "user_id = #{record.id}" }
 
-  has_many :worked_incedents, dependent: :destroy, foreign_key: 'worker_id'
+  has_many :owned_incedents, class_name: 'Incedent', dependent: :destroy, foreign_key: 'initiator_id'
+
+  before_destroy { |record| Incedent.update_all "initiator_id = 1", "initiator_id = #{record.id}" }
+
+  has_many :worked_incedents, class_name: 'Incedent', dependent: :destroy, foreign_key: 'worker_id'
+
+  before_destroy { |record| Incedent.update_all "worker_id = 1", "worker_id = #{record.id}" }
 
   has_many :incedent_actions, dependent: :destroy, foreign_key: 'worker_id'
 
+  before_destroy { |record| IncedentAction.update_all "worker_id = 1", "worker_id = #{record.id}" }
+
   has_many :comments, dependent: :destroy, foreign_key: 'author_id'
+
+  before_destroy { |record| Comment.update_all "author_id = 1", "author_id = #{record.id}" }
 
   validates_length_of :password, minimum: 3, message: 'Пароль должен быть длиннее 3 символов', if: :password
 
