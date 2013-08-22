650a53abc72796620bdd2c15ea907229eecff1ef
Alexey Kolosov <alexey.kolosov@gmail.com>
2013-08-22 17:30:35 +0600

    Версия 0.10.0

--- config/application.rb
+++ config/application.rb
@@ -68,7 +68,7 @@ module Houston
     config.incedent_waited   = 8
 
     config.app_name = 'Houston'
-    config.version = '0.9.3'
+    config.version = '0.10.0'
     config.email = 'houston@taxinonstop.ru'
   end
 end
