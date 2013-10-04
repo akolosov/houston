c4592cd6b48ac5269868b55ba8d8b52edfcc366c
Alexey Kolosov <alexey.kolosov@gmail.com>
2013-10-04 15:39:21 +0600

    Небольшие правки в комментах

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Houston::Application.configure do
 
   # ActionMailer settings
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:               'smtp.taxinonstop.ru',
