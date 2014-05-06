edbb851b271d8b4927fc674f681a86fd7ac4a7d5
Alexey Kolosov <alexey.kolosov@gmail.com>
2014-05-06 10:14:24 +0600

    fix

--- app/models/incedent.rb
+++ app/models/incedent.rb
@@ -469,7 +469,7 @@ class Incedent < ActiveRecord::Base
     User.active.each do |user|
       @incedents = Incedent.solved(false).by_worker(user)
       unless @incedents.empty?
-        IncedentMailer.delay({ run_at: 1.minutes.from_now }).incedents_in_progress(@incedents, user)
+        IncedentMailer.incedents_in_progress(@incedents, user).deliver
       end
     end
   end
